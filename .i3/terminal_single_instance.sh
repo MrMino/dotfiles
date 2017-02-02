5fcb5fd2ff2217fef843e5f691a28a7a6cb25552
Błażej Michalik <michalikblazej95@gmail.com>
2017-02-02 18:06:40 +0100

    Added single terminal instance script.

---
+++ .i3/terminal_single_instance.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+TERMINAL_NAME='gnome-terminal'
+count=`pgrep $TERMINAL_NAME | wc -l`
+echo "pgrep counted $count occurrence(s) of $TERMINAL_NAME"
+if [ $count -eq 0 ]; then
+	eval $TERMINAL_NAME
+else
+	i3-msg "[title=Terminal] focus"
+fi
