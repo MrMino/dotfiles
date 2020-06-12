ac8619f090a7ce999d2be3aba5188a4ffd8cc9f5
Blazej Michalik <blazej.michalik@nokia.com>
2020-06-12 02:03:31 +0200

    Improve go-terminal shortcut (alt + enter)

--- .i3/terminal_single_instance.sh
+++ .i3/terminal_single_instance.sh
@@ -5,5 +5,6 @@ echo "pgrep counted $count occurrence(s) of $TERMINAL_NAME"
 if [ $count -eq 0 ]; then
 	eval $TERMINAL_NAME
 else
-	i3-msg "[title=Terminal] focus"
+	i3-msg "[title=Terminal class=\"Gnome-terminal\"] focus"
+
 fi
